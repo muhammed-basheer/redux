a8703093a5c60c581c10e05f4e8fc937dfee195a
Basheer1994 <mu.basheer.tcs@gmail.com>
2025-03-01 11:31:09 +0530

    set the routes for all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from '../src/pages/Home'
+import About from '../src/pages/About'
+import Profile from '../src/pages/Profile'
+import SignIn from  '../src/pages/SignIn'
+import SignUp from '../src/pages/SignUp'
 
 function App() {
   return (
-    <div className='text-emerald-600'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/profile' element={<Profile/>} />
+      <Route path='/signin' element={<SignIn/>} />
+      <Route path='/signup' element={<SignUp/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
