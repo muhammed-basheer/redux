d16082cf3c9010cd43e8159afc0c75f4be2b9500
Basheer1994 <mu.basheer.tcs@gmail.com>
2025-03-03 15:29:24 +0530

    error handling and custom error handling

--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ app.listen(3000,()=>{
 
 app.use('/api/user',user_route)
 app.use('/api/auth',auth_route)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
+})
