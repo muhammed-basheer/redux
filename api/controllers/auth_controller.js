728db5f1b2fa288ea6b9a846655e74ca57a37294
Basheer1994 <mu.basheer.tcs@gmail.com>
2025-03-05 05:55:57 +0530

    create sign in functionality including jwt

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user_model.js";
 import bcryptjs from "bcryptjs";
 import errorHandling from '../utils/error.js';
+import jwt from "jsonwebtoken"
 
 export const signup = async(req,res,next)=>{
    const {username,email,password} = req.body;
@@ -12,5 +13,25 @@ export const signup = async(req,res,next)=>{
 
    } catch (error) {
     next(errorHandling(500,"something went wrong"))
-   }
+   } 
    }  
+
+   export const signin = async(req,res,next)=>{
+      const {email,password} = req.body;
+      try {
+
+         const validUser = await User.findOne({email:email})
+         if(!validUser) return next(errorHandling(401,"User Not Found"));
+
+         const validPassword = await bcryptjs.compareSync(password,validUser.password);
+         if(!validPassword) return next(errorHandling(403,"Wrong credentials"))
+
+         const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+              const {password:hashedPassword ,...rest} = validUser._doc;
+              const expiryDate = new Date(Date.now() + 3600000)  
+         res.cookie('access_token', token ,{httpOnly : true , expires : expiryDate}).status(200).json(rest)
+         
+      } catch (error) {
+         next(error)
+      }
+   }
--- api/routes/auth_route.js
+++ api/routes/auth_route.js
@@ -1,8 +1,10 @@
 import e from "express";
-import { signup } from "../controllers/auth_controller.js";
+import { signin, signup } from "../controllers/auth_controller.js";
 
 const router = e.Router();
 
 router.post('/signup',signup)
+router.post('/signin',signin)
+
 
 export default router;  
